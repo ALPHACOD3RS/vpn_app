e1ec547f3a4d127c1bb42475700475f3e6a45683
Naol Ketema <naolketema55@gmail.com>
2023-01-20 00:11:55 +0300

    lists are done

---
+++ lib/lists.dart
@@ -0,0 +1,31 @@
+List country = [
+  "United States",
+  "France",
+  "German",
+  "canada",
+  "Japan",
+];
+
+List loca = [
+  17,
+  13,
+  10,
+  4,
+  18,
+];
+
+List pinG = [
+  174,
+  73,
+  118,
+  214,
+  56,
+];
+
+List imag = [
+  "images/usa.png",
+  "images/france.png",
+  "images/german.png",
+  "images/canada.png",
+  "images/japan.png"
+];
